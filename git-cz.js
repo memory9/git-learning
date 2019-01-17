04cbec93691ad6cd52c20edfde1ab3dc012e3430
mingrui <474562318@qq.com>
2019-01-17 16:11:18 +0800

    fix(git commit): standerlize git committing

---
+++ git-cz.js
@@ -0,0 +1,9 @@
+const path =require('path')
+const bootstrap = require('commitizen/dist/cli/git-cz').bootstrap
+
+bootstrap({
+    cliPath: path.join(__dirname, './node_modules/commitizen'),
+    config: {
+      path: path.join(__dirname, "./node_modules/cz-conventional-changelog")
+    }
+  })
